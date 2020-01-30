67809049ae73920c3c7bdbf885454a34775f394a
5anchezzz <5an4e3.free@gmail.com>
2020-01-30 22:27:41 +0300

    done

--- task.rb
+++ task.rb
@@ -1,3 +1,4 @@
+require 'date'
 class Task < Post
 
   def initialize
@@ -6,10 +7,23 @@ class Task < Post
   end
 
   def read_from_console
+    puts "Что надо сделать?"
+    @text = STDIN.gets.chomp
+
+    puts "К какому числу? (в формате ДД.ММ.ГГГГ)"
+    input = STDIN.gets.chomp
+
+    @due_date = Date.parse(input)
 
   end
 
   def to_strings
+
+    time_string = "Создано #{@created_at.strftime("%Y.%m.%d, %H:%M:%S")} \n\r \n\r"
+
+    dead_line = "Крайний срок: #{@due_date}"
+
+    return [dead_line, @text, time_string]
 
   end
 
